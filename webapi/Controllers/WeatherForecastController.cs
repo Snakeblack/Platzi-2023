ba7949fd4b0a3e092e892ac118b8839b07dcb6f8
SN4KEBLACK <mretamozo@proton.me>
2023-01-12 19:00:00 +0100

    primer commit

---
+++ webapi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+  private static readonly string[] Summaries = new[]
+  {
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+  };
+
+  private readonly ILogger<WeatherForecastController> _logger;
+
+  private static List<WeatherForecast> ListWeatherForcast = new List<WeatherForecast>();
+
+  public WeatherForecastController(ILogger<WeatherForecastController> logger)
+  {
+    _logger = logger;
+
+    if (ListWeatherForcast == null || !ListWeatherForcast.Any())
+    {
+      ListWeatherForcast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+      {
+        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        TemperatureC = Random.Shared.Next(-20, 55),
+        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+      }).ToList();
+    }
+  }
+
+  [HttpGet(Name = "GetWeatherForecast")]
+  public IEnumerable<WeatherForecast> Get()
+  {
+    return ListWeatherForcast;
+  }
+
+  [HttpPost]
+  public IActionResult Post(WeatherForecast weatherForecast)
+  {
+    ListWeatherForcast.Add(weatherForecast);
+
+    return Ok();
+  }
+
+  [HttpDelete("{index}")]
+  public IActionResult Delete(int index)
+  {
+    ListWeatherForcast.RemoveAt(index);
+
+    return Ok();
+  }
+
+}
